2e459b4a36a9b42303b0e52f6deb0b21a6489bdf
Kim-GeonHo <65757344+Kim-GeonHo@users.noreply.github.com>
2021-01-26 02:21:46 +0900

    class for candy game

--- geonhokim/1.Brute_force_Search/사탕게임.py
+++ geonhokim/1.Brute_force_Search/사탕게임.py
@@ -1,76 +1,65 @@
 from typing import List
 
 class solution:
+    def __init__(self, n):
+        self.max_line = 0
+        self.n = n
+    
+
+    def width(self, XorI: int, candy_board):
+        temp_max_line = 0
+
+        for y in range(self.n - 1):
+            if candy_board[XorI][y] == candy_board[XorI][y+1]:
+                temp_max_line += 1
+
+                if temp_max_line + 1 >= self.max_line:
+                    self.max_line = temp_max_line + 1
+
+            else:
+                temp_max_line = 0
+
+
+    def height(self, YorJ: int, candy_board):
+        temp_max_line = 0
+
+        for x in range(self.n - 1):
+            if candy_board[x][YorJ] == candy_board[x+1][YorJ]:
+                temp_max_line += 1
+
+                if temp_max_line + 1 >= self.max_line:
+                    self.max_line = temp_max_line + 1
+
+            else:
+                temp_max_line = 0     
+
+
     def brute_force(self, candy_board: List[List[str]]) -> int:
-        max_line = 0
-        for i in range(n):
-            for j in range(n-1):
-                copy_board = candy_board
+        for i in range(self.n):
+            for j in range(self.n-1):
+                candy_board[i][j], candy_board[i][j+1] = candy_board[i][j+1], candy_board[i][j]
                 
-                temp = ''
-                temp = copy_board[i][j]
-                copy_board[i][j] = copy_board[i][j+1]
-                copy_board[i][j+1] = temp
+                self.width(i, candy_board)
+
+                for y in range(j, j + 1 + 1):
+                    self.height(y, candy_board)
+
+                candy_board[i][j], candy_board[i][j+1] = candy_board[i][j+1], candy_board[i][j]
                 
-                temp_max_line = 0
-
-                for y in range(n - 1):
-                    if copy_board[i][y] == copy_board[i][y+1]:
-                        temp_max_line += 1
-
-                        if temp_max_line > max_line:
-                            max_line = temp_max_line
-
-                    else:
-                        temp_max_line = 0
-
-                for x in range(n - 1):
-                    for y in range(j, j + 1 + 1):
-                        if copy_board[x][y] == copy_board[x+1][y]:
-                            temp_max_line += 1
-
-                            if temp_max_line > max_line:
-                                max_line = temp_max_line
-
-                        else:
-                            temp_max_line = 0
-
+        for i in range(self.n - 1):
+            for j in range(self.n):
+                candy_board[i][j], candy_board[i+1][j] = candy_board[i+1][j], candy_board[i][j]
                 
-        for i in range(n - 1):
-            for j in range(n):
-                copy_board = candy_board
-                
-                temp = ''
-                temp = copy_board[i][j]
-                copy_board[i+1][j] = copy_board[i][j]
-                copy_board[i+1][j] = temp
-                
-                temp_max_line = 0
-
-                for x in range(n - 1):
-                    if copy_board[x][j] == copy_board[x+1][j]:
-                        temp_max_line += 1
-
-                        if temp_max_line > max_line:
-                            max_line = temp_max_line
-
-                    else:
-                        temp_max_line = 0
+                self.height(j, candy_board)
 
                 for x in range(i, i + 1 + 1):
-                    for y in range(n - 1):
-                        if copy_board[x][y] == copy_board[x][y+1]:
-                            temp_max_line += 1
+                    self.width(x, candy_board)
 
-                            if temp_max_line > max_line:
-                                max_line = temp_max_line
 
-                        else:
-                            temp_max_line = 0
+                candy_board[i][j], candy_board[i+1][j] = candy_board[i+1][j], candy_board[i][j]
 
-        return max_line
+        return self.max_line
 
-sol = solution()
 
 n = int(input())
 
@@ -80,9 +69,6 @@ for i in range(n):
     row_i = list(input())
     candy_board.append(row_i)
 
+sol = solution(n)
 
 print(sol.brute_force(candy_board))
-
-
-
-
