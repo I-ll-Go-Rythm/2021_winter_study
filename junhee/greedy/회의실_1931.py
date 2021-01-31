b09e329a2299488526f58354c26ed7bdfbd00e83
kojunhee <poo9705@naver.com>
2021-01-31 22:01:18 +0900

    회의실완료

---
+++ junhee/greedy/회의실_1931.py
@@ -0,0 +1,18 @@
+number = int(input())
+time_list = []
+answer = 1 # 답은 최소 1
+for i in range(number):
+    time_list.append(list(map(int,input().split())))
+time_list.sort(key = lambda x : (x[1], x[0]))
+#---2차원 배열에 입력 후 2번째 숫자를 기준으로 오름차순 정렬, 
+# 1번째 숫자를 두 번째 기준으로 오름차순 정렬---
+temp = time_list[0][1]
+
+for i in range(number-1):
+    if time_list[i+1][1] == temp :
+        if temp == time_list[i+1][0]:
+            answer += 1
+    elif time_list[i+1][0]>=temp:
+        answer += 1
+        temp = time_list[i+1][1]
+print(answer)
