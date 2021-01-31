0264ebe0eeb278ce0c5c8788b4f431475d02130e
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2021-01-31 11:58:15 +0900

    repair

---
+++ geonhokim/2.Greedy_Algorithm/수리공항승.py
@@ -0,0 +1,33 @@
+from typing import List
+
+class Solution:
+    def myself(self, L: int, leak_list: List[int]) -> int:
+        cnt = 1
+
+        leak_list.sort()
+
+        i = 0
+        j = 1
+        while True:
+            if i == len(leak_list) - 1:
+                break
+            
+            if i + j > len(leak_list) - 1:
+                break
+            
+            if leak_list[i] - 0.5 + L >= leak_list[i + j] + 0.5:
+                j += 1
+            
+            else:
+                i, j = i + j, 1
+                cnt += 1
+                
+
+        return cnt
+
+sol = Solution()
+
+N, L = map(int, input().split())
+
+leak_list = list(map(int, input().split()))
+print(sol.myself(L, leak_list))
