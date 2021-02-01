92d9127047d63ea2ce998d90ee1ac5819e58fa9d
Suwhoan Lim <ludence1029@gmail.com>
2021-02-01 06:53:28 +0000

    I am pretty sure that this code is fully functional and is capable of doing what is it supposed to do. Nevertheless, due to Timeout issue, I am forced to use other data structure that fits within the time range. I shall come back tomorrow with new data structure to finish this program.

--- suwhoanlim/greedy/test.cpp
+++ suwhoanlim/greedy/test.cpp
@@ -1,30 +1,23 @@
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
-bool cmp(int *a, int *b) {
-    return a[1] < b[1]; // sort increasing order of end time
-}
 
 int main(void) {
-    int N, curr = 0. cnt = 0;
-    cin >> N;
-    int **arr = new int*[N];
-    for(int i = 0; i < N; i++) {
-        arr[i] = new int[2];
-        cin >> arr[i][0] >> arr[i][1];
-    }
-    sort(arr, arr + N, cmp);
-    
-    for(int i = 0; i < arr[N-1][1], i++) {
-        if(arr[i][0] < curr) continue;
-        curr = arr[i][1];
-        cnt++;
-    }
-    
-    cout << cnt << endl;
-    
-    for(int i = 0; i < N; i++) {
-        delete arr[i];
-    delete arr;
-    return 0;
+	int arr[10] = {5,2,7,1,4,6,3,9,8,0};
+
+	cout << arr << endl;
+	cout << arr + 9 <<endl;
+	cout << arr + 10 <<endl;
+	
+	cout << *arr << endl;
+	cout << *(arr + 9) <<endl;
+	cout << *(arr + 10) <<endl;
+
+	sort(arr, arr+10);
+
+	cout << *arr << endl;
+	cout << *(arr + 9) <<endl;
+	cout << *(arr + 10) <<endl;
+	return 0;
 }
