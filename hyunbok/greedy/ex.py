8ae1e513b94ba62fb50af9a19589c5ea195512db
hyunbok <hyunbok>
2021-02-02 11:30:47 +0900

    21.02.02

--- hyunbok/greedy/ex.py
+++ hyunbok/greedy/ex.py
@@ -1,12 +1,3 @@
-k = 1
-a_k = []
-a_b = []
-a_l = []
-while k = 1:
-    k, b, l = map(int, input().split())
-    if k == 0 and b == 0 and l == 0 : break
-    a_k.append(k)
-    a_b.append(b)
-    a_l.append(l)
-
-for 
+a = [1,2,3,4]
+a.remove(1)
+print(a)
