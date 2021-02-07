eadcf103a5010a9201c231d3323770c2fca14d40
Suwhoan Lim <ludence1029@gmail.com>
2021-02-07 10:54:47 +0900

    1st done, don't forget the array bound

---
+++ suwhoanlim/divideAndConquer/2104_partialSum.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+
+ll solve(int start, int end, vector<int> const &arr) {
+    if(start == end) return (ll) arr[start] * arr[start];
+    
+    int mid = (start+end)/2; // use lower one
+    ll maxval = max(solve(start, mid, arr), solve(mid+1, end, arr));
+    
+    int left = mid, right = mid+1;
+    int minval = min(arr[left], arr[right]);
+    ll interSum = arr[left] + arr[right];
+    maxval = max(maxval, interSum * minval);
+    
+    while(start < left || right < end) {
+        if(right < end && (start == left || arr[left - 1] < arr[right + 1])) {
+            interSum += arr[++right];
+            minval = min(minval, arr[right]);
+        }
+        else {
+            interSum += arr[--left];
+            minval = min(minval, arr[left]);
+        }
+        maxval = max(maxval, interSum*minval);
+    }
+    
+    return maxval;
+}
+
+
+int main(void) {
+    int N;
+    ll sol;
+    cin >> N;
+    vector<int> arr(N);
+    for(int i = 0; i < N; i ++)
+        cin >> arr[i];
+    
+    sol = solve(0, N - 1, arr);
+    
+    cout << sol << '\n';
+    
+    return 0;    
+}
