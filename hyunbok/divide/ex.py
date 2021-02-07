dcbfd001c634fd96938cd9228a69f6545545e2d6
hyunbok <gusqhr2100@gmail.com>
2021-02-07 17:48:09 +0900

    21.02.07

--- hyunbok/divide/ex.py
+++ hyunbok/divide/ex.py
@@ -1,10 +1,2 @@
-a, b = map(int, input().split())
-def sol(a, b):
-    if a == 1 and b == 1:
-        return('right!')
-        
-    for i in range(a):
-        for j in range(b):
-            sol(1, 1)
-    return
-print(sol(a, b))
+a = [1, 2]
+print(type(a[1]))
