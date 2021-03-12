c0f3e32ec10bd92ba31401a553b008f6fd8e46b6
hy01n <17hyoinjeong@gm.gist.ac.kr>
2021-03-12 16:23:30 +0900

    tutorial

---
+++ hyoinjeong/PrefixSum/prefixsum4_11659.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main(){
+    int n,m;
+    scanf("%d %d",&n,&m);
+    int arr[n],ps[n+1]={0};
+    for(int i=0;i<n;i++){
+        scanf("%d",arr+i);
+        ps[i+1]=ps[i]+arr[i];
+    }
+    for(int i=0;i<m;i++){
+        int s,e;
+        scanf("%d %d",&s,&e);
+        printf("%d\n",ps[e]-ps[s-1]);
+    }
+    return 0;
+}
