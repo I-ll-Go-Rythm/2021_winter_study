a6c9473b22fc77a3c8563848854cdc79a02a4375
hy01n <17hyoinjeong@gm.gist.ac.kr>
2021-03-17 19:57:54 +0900

    3.17

--- hyoinjeong/PrefixSum/HCI_16139.cpp
+++ hyoinjeong/PrefixSum/HCI_16139.cpp
@@ -1,26 +1,29 @@
 #include <cstdio>
 #include <cstring>
 
-char arr[26][200001];
+
 
 int main(){
-    char t;
-    for(int i=0;t!=NULL;i++){
-        t=getchar();
-        arr[t-'a'][i]=1;
+    
+    //memset(arr,0,sizeof(arr));
+    //main에서 segmentation fault 뜨는 이유는 뭘까. int로 바꿔서 빌드하면 main에서 seg fault 오류가 난다.
+    int arr[26][200001]={0,};
+    for(int i=0;;i++){
+        char t = getchar();
+        if(t=='\n') break;
+        //이 부분에서 틀렸었다. 45프로에서 멈췄었는데 그 이유는 한번 더 반복문을 안써주면 부분합이 안되고 1 0 1 이렇게 될 수 있기 때문이다.
+        for(int j=0;j<26;j++)
+        arr[j][i+1]=arr[j][i]+(t=='a'+j?1:0);
     }
 
-    for(int j=1;j<)
     int q;
-    int f[n+1]={0},s[n+1]={0},t[n+1]={0};
-    int num;
-    for(int i=1;i<=n;i++){
-        
-    }
+    scanf("%d",&q);
+   
     for(int i=0;i<q;i++){
+        char al;
         int st,en;
-        scanf("%d %d",&st,&en);
-        printf("%d %d %d\n",f[en]-f[st-1],s[en]-s[st-1],t[en]-t[st-1]);
+        scanf(" %c %d %d",&al ,&st,&en);
+        printf("%d\n",arr[al-'a'][en+1]-arr[al-'a'][st]);
     }
     return 0;
 }
